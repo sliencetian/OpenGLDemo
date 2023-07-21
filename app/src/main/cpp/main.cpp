fece4d16124e396fe2e9d871f07508e8e4a5136e
tianzhao.max <tianzhao.max@bytedance.com>
2023-07-21 15:19:20 +0800

    init native activity

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -4,52 +4,28 @@
 
 #include <android_native_app_glue.h>
 #include <jni.h>
-
-#include "Layer.h"
-#include "Render.h"
 #include "Logger.h"
 
-struct AppData {
-    bool isPause = false;
-    Render* render = nullptr;
-    Layer* layer = new Layer();
-};
-
 static int32_t handle_input(struct android_app* app, AInputEvent* event) {
-    auto *appData = reinterpret_cast<AppData*>(app->userData);
-    if (!appData->isPause) {
-        appData->render->handleInput(event);
-    }
     return 0;
 }
 
 static void handle_cmd(struct android_app *app, int32_t cmd) {
-    auto *appData = reinterpret_cast<AppData*>(app->userData);
+
     switch (cmd) {
         case APP_CMD_INIT_WINDOW: {
-            LOGI("APP_CMD_INIT_WINDOW")
-            if (appData->render) {
-                appData->render->initSurface(app);
-            } else {
-                appData->render = new Render(app,appData->layer);
-            }
             break;
         }
         case APP_CMD_TERM_WINDOW: {
             LOGI("APP_CMD_TERM_WINDOW")
-            if (appData->render) {
-                appData->render->destroySurface();
-            }
             break;
         }
         case APP_CMD_RESUME: {
             LOGI("APP_CMD_RESUME")
-            appData->isPause = false;
             break;
         }
         case APP_CMD_PAUSE: {
             LOGI("APP_CMD_PAUSE")
-            appData->isPause = true;
             break;
         }
         case APP_CMD_STOP: {
@@ -58,7 +34,6 @@ static void handle_cmd(struct android_app *app, int32_t cmd) {
         }
         case APP_CMD_DESTROY: {
             LOGI("APP_CMD_DESTROY")
-            delete appData->render;
             break;
         }
         default:
@@ -68,13 +43,6 @@ static void handle_cmd(struct android_app *app, int32_t cmd) {
 
 void android_main(struct android_app *app) {
     LOGI("Welcome to android_main")
-    AppData appData{};
-    // 添加渲染的 layer
-    appData.layer->layers.push_back(new TemplateLayer());
-    appData.layer->layers.push_back(new BackgroundLayer());
-    appData.layer->layers.push_back(new TriangleLayer());
-    // 保存当前数据
-    app->userData = &appData;
 
     app->onAppCmd = handle_cmd;
     app->onInputEvent = handle_input;
@@ -88,11 +56,6 @@ void android_main(struct android_app *app) {
             if (source) {
                 source->process(app, source);
             }
-        }
-        // do app something
-        if (!appData.isPause && appData.render != nullptr) {
-            // Render a frame
-            appData.render->render();
         }
     }
 }
